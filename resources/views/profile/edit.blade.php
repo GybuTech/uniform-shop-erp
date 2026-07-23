8eb912db3c781dc6e85d660f9095027ce29e7b79
Gabriel Mbuguaa <gabrielmbugua003@email.com>
2026-07-23 06:59:42 +0300

    fix: disable account deletion UI and restore correct route configuration

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,3 +1,4 @@
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -20,9 +21,7 @@
             </div>
 
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
+                @include('profile.partials.delete-user-form')
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/reports', function () {
-        return view('dashboard');
-    })->middleware('permission:view-reports')->name('reports');
 });
 
 require __DIR__.'/auth.php';
